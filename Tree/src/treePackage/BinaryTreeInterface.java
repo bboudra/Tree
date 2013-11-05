984632f245083c663ee31e966c2aeb8a7f3bd9dc
Benjamin Boudra <benboudra@outlook.com>
2013-11-05 11:33:18 -0700

    V 1.2
    Finished the interfaces.

--- Tree/src/treePackage/BinaryTreeInterface.java
+++ Tree/src/treePackage/BinaryTreeInterface.java
@@ -1,6 +1,24 @@
 package treePackage;
 
-public class BinaryTreeInterface <E> extends treeInterface<E>
+public interface BinaryTreeInterface<E> extends TreeInterface<E>,
+		TreeIteratorInterface<E>
 {
+	/**
+	 * sets this binary tree to a new one-node binary tree.
+	 * 
+	 * @param rootData
+	 *            an object that is the data in the new tree's root.
+	 * 
+	 */
+	public void setTree(E rootData);
 	
+
+	/**
+	 * Sets this binary tree to a new binary tree.
+	 * 
+	 * @param rootData - An object that is the data in the new tree's root
+	 * @param leftTree - The left subtree of the new tree
+	 * @param rightTree - The right subtree of the new tree
+	 */
+	public void setTree(E rootData, BinaryTreeInterface<E> leftTree, BinaryTreeInterface<E> rightTree);
 }
--- Tree/src/treePackage/TreeIteratorInterface.java
+++ Tree/src/treePackage/TreeIteratorInterface.java
@@ -1,5 +1,5 @@
 package treePackage;
-
+import java.util.Iterator;
 /**
  * 
  * @author Ben Boudra
